19761fb106d99775d40247820b7d24abac3e2dca
VoTanTai.dp <votantai27072003@gmail.com>
2025-12-07 11:31:04 +0700

    init components/camera

---
+++ front-end/src/components/Camera/Camera.js
@@ -0,0 +1,12 @@
+import './Camera.scss';
+import { useEffect } from "react"
+
+const Camera = (props) => {
+    return (
+        <div>
+            <h1>Camera</h1>
+        </div>
+    )
+}
+
+export default Camera
